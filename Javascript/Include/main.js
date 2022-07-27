3f2b3a2c0587c22609d5f3be2cc047dade33e831
Hemanth Kumar <90673550+Hemanthoo7@users.noreply.github.com>
2022-07-27 20:03:05 +0530

    JS Practiced code with some examples
    JS Practiced code with some examples

---
+++ Javascript/Include/main.js
@@ -0,0 +1,9 @@
+// import the variables and function from module.js
+import { message, number, multiplyNumbers } from './modules.js';
+
+console.log(message); // hello world
+
+console.log(number); // 10
+
+document.write(multiplyNumbers(3, 4)); // 12
+document.write(multiplyNumbers(5, 8)); // 40
